f008d70cb0efe2dd5b9f92b527f331bff24afc71
Dag Rende <dag.rende@find-out.se>
2012-12-09 17:28:20 +0100

    added toolbar, but tools with only icon are not visible

--- MyProject/src/mypackage/client/MyProject.java
+++ MyProject/src/mypackage/client/MyProject.java
@@ -3,31 +3,57 @@ package mypackage.client;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.gef.EditDomain;
+import org.eclipse.gef.ReflectionHelper;
+import org.eclipse.gef.examples.shapes.PaletteHacks;
 import org.eclipse.gef.examples.shapes.model.EllipticalShape;
 import org.eclipse.gef.examples.shapes.model.RectangularShape;
 import org.eclipse.gef.examples.shapes.model.ShapesDiagram;
 import org.eclipse.gef.examples.shapes.parts.ShapesEditPartFactory;
+import org.eclipse.gef.palette.PaletteRoot;
+import org.eclipse.gef.ui.palette.PaletteViewer;
+import org.eclipse.gef.ui.palette.PaletteViewerProvider;
 import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class MyProject implements EntryPoint {
+	EditDomain editDomain = new EditDomain();
+
 	@Override
 	public void onModuleLoad() {
+		createInstantiators();
+
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+
+		// create tool palette to the left of the shapes editor and connect to
+		// it
+		Composite paletteControl = new Composite(null, SWT.NONE);
+		PaletteRoot palette = PaletteHacks.getPalette();
+		PaletteViewerProvider paletteViewerProvider = new PaletteViewerProvider(
+				editDomain);
+		PaletteViewer paletteViewer = paletteViewerProvider
+				.createPaletteViewer(paletteControl);
+		paletteViewer.setContents(palette);
+		paletteControl.setSize(100, 300);
+		Widget paletteParentGwtWidget = paletteControl.getGwtWidget();
+		horizontalPanel.add(paletteParentGwtWidget);
+
+		Composite editorControl = new Composite(null, SWT.NONE);
 		ScrollingGraphicalViewer sgv = new ScrollingGraphicalViewer();
-		
-		Composite composite = new Composite(null, SWT.NONE);
-		
-		sgv.createControl(composite);
+		sgv.createControl(editorControl);
 		sgv.setEditPartFactory(new ShapesEditPartFactory());
-		sgv.setEditDomain(new EditDomain());
+		sgv.setEditDomain(editDomain);
 		sgv.setContents(createContent());
-		Widget gwtWidget = composite.getGwtWidget();
-		RootLayoutPanel.get().add(gwtWidget);
+		editorControl.setSize(700, 300);
+		Widget gwtWidget = editorControl.getGwtWidget();
+		horizontalPanel.add(gwtWidget);
+
+		RootLayoutPanel.get().add(horizontalPanel);
 	}
 
 	private Object createContent() {
@@ -42,4 +68,32 @@ public class MyProject implements EntryPoint {
 		diagram.addChild(es);
 		return diagram;
 	}
+
+	private void createInstantiators() {
+		ReflectionHelper.registerHelper(
+				org.eclipse.gef.tools.CreationTool.class,
+				new ReflectionHelper.Instantiator() {
+					@Override
+					public Object newInstance(Class c) {
+						return new org.eclipse.gef.tools.CreationTool();
+					}
+				});
+		ReflectionHelper.registerHelper(
+				org.eclipse.gef.examples.shapes.model.EllipticalShape.class,
+				new ReflectionHelper.Instantiator() {
+					@Override
+					public Object newInstance(Class c) {
+						return new org.eclipse.gef.examples.shapes.model.EllipticalShape();
+					}
+				});
+		ReflectionHelper.registerHelper(
+				org.eclipse.gef.examples.shapes.model.RectangularShape.class,
+				new ReflectionHelper.Instantiator() {
+					@Override
+					public Object newInstance(Class c) {
+						return new org.eclipse.gef.examples.shapes.model.RectangularShape();
+					}
+				});
+	}
+
 }
